5ca6d773d8288d57fb823bcfc8a3076d8a6af68c
Vitor-Hott-Prado <vitorhott061@gmail.com>
2025-02-25 11:26:25 -0300

    25.02.2025

---
+++ ArrayMatrix/Exercicios.js
@@ -0,0 +1,15 @@
+// Exercio 1
+
+const prompt = require("prompt-sync")();
+
+
+
+let arrayTexto = ["maçã", "banana", "laranja", "uva"];
+
+console.log(arrayTexto[1]);
+arrayTexto.push("melancia");
+console.log(arrayTexto); 
+
+arrayTexto.shift();
+console.log(arrayTexto); 
+console.log(arrayTexto.length); 
