9c8e1ebbdccabde7ca3f57301b9429eda0e598d7
Vitor-Hott-Prado <vitorhott061@gmail.com>
2025-08-20 13:25:44 -0300

    comenario

--- FrameWorks/Angular16/projeto-auth/src/app/servicos/auth.service.ts
+++ FrameWorks/Angular16/projeto-auth/src/app/servicos/auth.service.ts
@@ -16,30 +16,38 @@ export class AuthService {
 
 //Métodos 
 
-//Registar
+//fUNÇAO PARA REGISTRAR UM NO USUARIO
 
   registrar(usuario: any):Observable<any> {
+    // antes de registrar -> verificar se ususario já esta  cadastrado
     return this.http.get<any[]>(`${this.apiUrl}?email=${usuario.email}`).pipe(
       switchMap(res => {
         if(res.length>0){
-          return throwError(()=>new Error ("Usuário Já Existe."));
+          //criando um erro para o sistema -> para que o erro seja tratado, rodar a função dentro de um try/cath
+          return throwError(()=>new Error ("Usuário Já Existe.")); // paraa o programa aqui, se ususario já existi
         }
+        //caso contrário -> cadastrar o usuário novo
         return this.http.post<any>(this.apiUrl, usuario);
       })
     );
   }
 
 
-// Login 
+// Login - Função
 
   login(credenciais: any):Observable<boolean>{
+
     return this.http.get<any[]>(
+     //paasar  oemail e a senha para pocuar o usuario no BD
       `${this.apiUrl}?email=${credenciais.email}&senha=${credenciais.senha}`).pipe(
         map(usuario => {
+          //usuario encontrado
           if (usuario.length > 0) {
+                  // grava no aramazenamento interno do navegador(a chave de autenticação e as informaç~eos do usuário)
             localStorage.setItem(this.CHAVE_AUTH, JSON.stringify(usuario[0]));
             return true;
           }
+          //caso não seja encontrado - > retorna falso 
           return false;
         })
     );
@@ -48,21 +56,26 @@ export class AuthService {
 //Logout
 
 lougout(){
+  //lompo o armazenamento interno
   localStorage.removeItem(this.CHAVE_AUTH);
+  //redirecionar para tela de logn/ home
   this.router.navigate(["/login"]);
 }
 
 
-// Esta Autenticado
+// Esta Autenticado - verificar se o usuario esta autenticado
 
 estaAutenticado(): boolean{
+  //uso de dupla negação => transformo em uma booleana
+  // !! - > retorna vazio - > FontFaceSetLoadEvent, agora se torna texto - é true
   return !!localStorage.getItem(this.CHAVE_AUTH);
 
 }
 
-//Usuario Atuatal
+//Usuario função para pegar as informaç~~oes do usuario 
 
 usuarioAtual(): any{
+  //conveerte para json as informações do usurio
   return JSON.parse(localStorage.getItem(this.CHAVE_AUTH)|| "{}")
   
 }
